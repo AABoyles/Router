59b16c54fdf0588e083ee7b337f4fbac670e2127
AABoyles <aaboyles@gmail.com>
2014-06-17 04:11:33 +0000

    Second Commit, cleanup and optimizations.

--- js/map.js
+++ js/map.js
@@ -1,29 +1,78 @@
-app = {};
-$(function() {
+app = {
+    distances: []
+};
+function init(){
 	app.map = new OpenLayers.Map('map');
 	app.map.addLayer(new OpenLayers.Layer.OSM("Open Street Map"));
 
 	app.pathLayer = new OpenLayers.Layer.Vector("Path Layer");
 	app.path = new OpenLayers.Geometry.LinearRing([]);
-	app.pathLayer.addFeatures([new OpenLayers.Feature.Vector(app.path, {}, {fillColor: "#996600", fillOpacity: .3})]);
+	app.pathLayer.addFeatures([new OpenLayers.Feature.Vector(app.path, {}, {fillOpacity:0})]);
 	app.map.addLayer(app.pathLayer);
 
 	app.drawingLayer = new OpenLayers.Layer.Vector("Drawing Layer");
 	app.map.addLayer(app.drawingLayer);
 
+    app.addVertex = function(vertexA){
+        var vertices = app.path.getVertices();
+        var z = [];
+        vertices.forEach(function(vertexB, b){
+            var dist = vertexA.distanceTo(vertexB);
+            app.distances[b].push(dist);
+            z.push(dist);
+        });
+        z.push(0);
+        app.distances.push(z);
+        app.path.addComponent(vertexA);
+		app.pathLayer.redraw();
+    };
+
 	app.recalcRoute = function(){
+        var vertices = app.path.getVertices();
+        vertices.forEach(function(vertexA,a){app.distances[a]=[];});
+        var max = 0;
+        vertices.every(function(vertexA,a){
+            vertices.every(function(vertexB, b){
+                if(b==a){
+                    app.distances[a].push(0);
+                    return false;
+                }
+                var dist = vertexA.distanceTo(vertexB);
+                app.distances[a].push(dist);
+                app.distances[b].push(dist);
+                if(dist > max){ max = dist; }
+                return true;
+            });
+            return true;
+        });
+        
 		app.pathLayer.redraw();
 	};
 
+    app.removeVertex = function(vertexA){
+        var vertices = app.path.getVertices();
+        var remove = 0;
+        vertices.every(function(vertexB, b){
+            if(_.isEqual(vertexA, vertexB)){
+                remove = b;
+                app.distances.splice(remove,1);
+        		return false;
+            }
+        });
+        vertices.every(function(vertexB, b){
+            app.distances[b].splice(remove,1);
+        });
+        app.path.removeComponent(vertexA);
+		app.pathLayer.redraw();
+    };
+    
 	app.drawingLayer.events.register("featureremoved", {}, function(evt){
-		app.path.removeComponent(evt.feature.geometry);
-		app.recalcRoute();
+		app.removeVertex(evt.feature.geometry);
 	});
 
 	var point = new OpenLayers.Control.DrawFeature(app.drawingLayer, OpenLayers.Handler.Point);
 	point.events.register("featureadded", {}, function(evt){
-		app.path.addComponent(evt.feature.geometry);
-		app.recalcRoute();
+		app.addVertex(evt.feature.geometry);
 	});
 
 	var drag = new OpenLayers.Control.DragFeature(app.drawingLayer, {
@@ -43,4 +92,4 @@ $(function() {
 	drag.activate();
 
 	app.map.zoomToMaxExtent();
-});
+};
