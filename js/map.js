045c66476c4079f38a1cd70c6307e14befa7fab8
AABoyles <aaboyles@gmail.com>
2014-06-18 17:27:05 +0000

    Fixed Path-finding algorithm.  It now finds a Path, which may or may not be cyclical.  Still a long way to go, but progress!

--- js/map.js
+++ js/map.js
@@ -1,95 +1,74 @@
-app = {
-    distances: []
+app={
+  map: {},
+  path: {},
+  pathLayer: {},
+  drawingLayer: {},
+  recalcRoute: function(){}
 };
+
 function init(){
-	app.map = new OpenLayers.Map('map');
-	app.map.addLayer(new OpenLayers.Layer.OSM("Open Street Map"));
+  app.map = new OpenLayers.Map("map");
+  app.map.addLayer(new OpenLayers.Layer.OSM("Open Street Map"));
 
-	app.pathLayer = new OpenLayers.Layer.Vector("Path Layer");
-	app.path = new OpenLayers.Geometry.LinearRing([]);
-	app.pathLayer.addFeatures([new OpenLayers.Feature.Vector(app.path, {}, {fillOpacity:0})]);
-	app.map.addLayer(app.pathLayer);
+  app.drawingLayer = new OpenLayers.Layer.Vector("Drawing Layer");
+  app.map.addLayer(app.drawingLayer);
 
-	app.drawingLayer = new OpenLayers.Layer.Vector("Drawing Layer");
-	app.map.addLayer(app.drawingLayer);
+  app.path = new OpenLayers.Geometry.LineString([]);
+  app.pathLayer = new OpenLayers.Layer.Vector("Path Layer");
+  app.pathLayer.addFeatures([new OpenLayers.Feature.Vector(app.path, {}, {
+    fillOpacity: 0
+  })]);
+  app.map.addLayer(app.pathLayer);
 
-    app.addVertex = function(vertexA){
-        var vertices = app.path.getVertices();
-        var z = [];
-        vertices.forEach(function(vertexB, b){
-            var dist = vertexA.distanceTo(vertexB);
-            app.distances[b].push(dist);
-            z.push(dist);
-        });
-        z.push(0);
-        app.distances.push(z);
-        app.path.addComponent(vertexA);
-		app.pathLayer.redraw();
-    };
+  app.recalcRoute = function() {
+    var vertices = _.pluck(app.drawingLayer.getFeaturesByAttribute(), "geometry");
+    app.path.removeComponents(vertices);
+    for(var i = 0; i < vertices.length; i++){
+      var activeVertices = app.path.components;
+      var minInsertDist = Number.MAX_VALUE;
+      var insertIndex = 0;
+      for(var j = 0; j < activeVertices.length-1; j++){
+        var before = vertices[j], after = vertices[j+1];
+        if(before == vertices[i] || after == vertices[i]) continue;
+        var insertDist = before.distanceTo(vertices[i]) + after.distanceTo(vertices[i]) - before.distanceTo(after);
+        if(insertDist < minInsertDist){
+          minInsertDist = insertDist;
+          insertIndex = i+1;
+        }
+      }
+      app.path.addComponent(vertices[i], insertIndex);
+    }
+    app.pathLayer.redraw();
+  };
 
-	app.recalcRoute = function(){
-        var vertices = app.path.getVertices();
-        vertices.forEach(function(vertexA,a){app.distances[a]=[];});
-        var max = 0;
-        vertices.every(function(vertexA,a){
-            vertices.every(function(vertexB, b){
-                if(b==a){
-                    app.distances[a].push(0);
-                    return false;
-                }
-                var dist = vertexA.distanceTo(vertexB);
-                app.distances[a].push(dist);
-                app.distances[b].push(dist);
-                if(dist > max){ max = dist; }
-                return true;
-            });
-            return true;
-        });
-        
-		app.pathLayer.redraw();
-	};
+  app.drawingLayer.events.register("featureremoved", {}, function(evt) {
+    app.path.removeComponent(evt.feature.geometry);
+    app.recalcRoute();
+  });
 
-    app.removeVertex = function(vertexA){
-        var vertices = app.path.getVertices();
-        var remove = 0;
-        vertices.every(function(vertexB, b){
-            if(_.isEqual(vertexA, vertexB)){
-                remove = b;
-                app.distances.splice(remove,1);
-        		return false;
-            }
-        });
-        vertices.every(function(vertexB, b){
-            app.distances[b].splice(remove,1);
-        });
-        app.path.removeComponent(vertexA);
-		app.pathLayer.redraw();
-    };
-    
-	app.drawingLayer.events.register("featureremoved", {}, function(evt){
-		app.removeVertex(evt.feature.geometry);
-	});
+  var point = new OpenLayers.Control.DrawFeature(app.drawingLayer, OpenLayers.Handler.Point);
+  point.events.register("featureadded", {}, function(evt) {
+    app.path.addComponent(evt.feature.geometry);
+    app.recalcRoute();
+  });
 
-	var point = new OpenLayers.Control.DrawFeature(app.drawingLayer, OpenLayers.Handler.Point);
-	point.events.register("featureadded", {}, function(evt){
-		app.addVertex(evt.feature.geometry);
-	});
+  var drag = new OpenLayers.Control.DragFeature(app.drawingLayer, {
+    onDrag: function(feature) {
+      app.recalcRoute();
+    },
+  });
 
-	var drag = new OpenLayers.Control.DragFeature(app.drawingLayer, {
-		onDrag: function(feature){
-			app.recalcRoute();
-		},
-	});
+  app.map.addControls([
+    point,
+    drag,
+    new OpenLayers.Control.LayerSwitcher(),
+    new OpenLayers.Control.MousePosition({
+      displayProjection: "EPSG:4326"
+    })
+  ]);
 
-	app.map.addControls([
-		point,
-		drag,
-		new OpenLayers.Control.LayerSwitcher(),
-		new OpenLayers.Control.MousePosition({displayProjection:"EPSG:4326"})
-	]);
+  point.activate();
+  drag.activate();
 
-	point.activate();
-	drag.activate();
-
-	app.map.zoomToMaxExtent();
+  app.map.zoomToMaxExtent();
 };
