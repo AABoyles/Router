fe1016938f0845411e9a4791ce37712efaa3f7eb
AABoyles <aaboyles@gmail.com>
2014-06-24 14:56:05 +0000

    Initial build using OL3. Algorithm is entirely unimplemented.

--- js/map.js
+++ js/map.js
@@ -1,54 +1,40 @@
 app = {
-    interactions: {},
     map: {},
-    path: {},
-    pathLayer: {},
-    drawingLayer: {},
+    featureOverlay: {},
     recalcRoute: function() {}
 };
 
 function init() {
-    var raster = new ol.layer.Tile({
-        source: new ol.source.MapQuest({
-            layer: 'sat'
-        })
-    });
-    var map = new ol.Map({
-        layers: [raster],
+    app.map = new ol.Map({
+        layers: [new ol.layer.Tile({
+			source: new ol.source.OSM()
+		})],
         target: 'map',
         view: new ol.View2D({
             center: [-11000000, 4600000],
             zoom: 4
         })
     });
-    var featureOverlay = new ol.FeatureOverlay({
+    app.featureOverlay = new ol.FeatureOverlay({
         style: new ol.style.Style({
-            fill: new ol.style.Fill({
-                color: 'rgba(255, 255, 255, 0.2)'
-            }),
             stroke: new ol.style.Stroke({
-                color: '#ffcc33',
+                color: '#0000ff',
                 width: 2
-            }),
-            image: new ol.style.Circle({
-                radius: 7,
-                fill: new ol.style.Fill({
-                    color: '#ffcc33'
-                })
             })
         })
     });
-    featureOverlay.setMap(map);
-    var modify = new ol.interaction.Modify({
-        features: featureOverlay.getFeatures(),
+    app.featureOverlay.setMap(app.map);
+    app.map.addInteraction(new ol.interaction.Modify({
+        features: app.featureOverlay.getFeatures(),
         deleteCondition: function(event) {
             return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
         }
-    });
-    map.addInteraction(modify);
-    var draw = new ol.interaction.Draw({
-        features: featureOverlay.getFeatures(),
+    }));
+    app.map.addInteraction(new ol.interaction.Draw({
+        features: app.featureOverlay.getFeatures(),
         type: "Polygon"
-    });
-    map.addInteraction(draw);
-};
+    }));
+    app.recalcRoute = function(){
+        
+    };
+}
