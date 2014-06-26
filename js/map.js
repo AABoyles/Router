98a144b663f2596b9c892d7e138a770d8d1c14e3
AABoyles <aaboyles@gmail.com>
2014-06-26 01:20:43 +0000

    Initial branch with D3, doesnt do anything

--- js/map.js
+++ js/map.js
@@ -10,115 +10,20 @@ app = {
     neCalcRoute: function() {},
 };
 $(function() {
-    app.pointLayer = new OpenLayers.Layer.Vector("Points");
-    app.nnPath = new OpenLayers.Geometry.LinearRing();
-    app.nnPathLayer = new OpenLayers.Layer.Vector("Nearest-Neighbor");
-    app.nnPathLayer.addFeatures([new OpenLayers.Feature.Vector(app.nnPath, {}, {
-        fillOpacity: 0,
-        strokeColor: "#ff0000"
-    })]);
-    app.nePath = new OpenLayers.Geometry.LinearRing();
-    app.nePathLayer = new OpenLayers.Layer.Vector("Nearest-Edge");
-    app.nePathLayer.addFeatures([new OpenLayers.Feature.Vector(app.nePath, {}, {
-        fillOpacity: 0,
-        strokeColor: "#0000ff"
-    })]);
-    app.map = new OpenLayers.Map({
-        div: "map",
-        layers: [new OpenLayers.Layer.OSM("Open Street Map"), app.pointLayer, app.nnPathLayer, app.nePathLayer],
-        center: [-10973646.505861, 4841142.9262657],
-        zoom: 5
+    var win = $("window");
+    var width = win.width(), height = win.height();
+    var projection = d3.geo.equirectangular().scale(153).translate([width / 2, height / 2]).precision(.1);
+    var path = d3.geo.path().projection(projection);
+    var graticule = d3.geo.graticule();
+    var svg = d3.select("map").append("svg").attr("width", width).attr("height", height);
+    svg.append("path").datum(graticule).attr("class", "graticule").attr("d", path);
+    d3.json("/mbostock/raw/4090846/world-50m.json", function(error, world) {
+        svg.insert("path", ".graticule").datum(topojson.feature(world, world.objects.land)).attr("class", "land").attr("d", path);
+        svg.insert("path", ".graticule").datum(topojson.mesh(world, world.objects.countries, function(a, b) {
+            return a !== b;
+        })).attr("class", "boundary").attr("d", path);
     });
-    app.neCalcRoute = function() {
-        var vertices = _.pluck(app.pointLayer.getFeaturesByAttribute(), "geometry");
-        app.nePath.removeComponents(vertices);
-        vertices.forEach(function(vertex) {
-            var activeVertices = app.nePath.components;
-            if(_.contains(activeVertices, vertex)) return;
-            var minInsertDist = Number.MAX_VALUE;
-            var insertIndex = activeVertices.length;
-            for(var j = 1; j < activeVertices.length; j++) {
-                var before = activeVertices[j - 1],
-                    after = activeVertices[j];
-                if(vertex == before || vertex == after) continue;
-                var insertDist = before.distanceTo(vertex) + vertex.distanceTo(after) - before.distanceTo(after);
-                if(insertDist < minInsertDist) {
-                    minInsertDist = insertDist;
-                    insertIndex = j;
-                }
-            }
-            app.nePath.addComponent(vertex, insertIndex);
-        });
-        app.nePathLayer.redraw();
-        var km = app.nePath.getLength()/1000 + "";
-        $("#nearest-edge-distance").text(km.substring(0, km.indexOf(".")) + " km");
-    };
-    app.nnCalcRoute = function(n) {
-        n = typeof n === 'number' ? n : 0;
-        var vertices = _.pluck(app.pointLayer.getFeaturesByAttribute(), "geometry");
-        app.nnPath.removeComponents(vertices);
-        var current = vertices[n];
-        while(true) {
-            var unvisitedVertices = _.difference(vertices, app.nnPath.components);
-            if(unvisitedVertices.length == 0) {
-                break;
-            }
-            var minInsertDist = Number.MAX_VALUE;
-            unvisitedVertices.forEach(function(next) {
-                var insertDist = current.distanceTo(next);
-                if(insertDist < minInsertDist) {
-                    nextNode = next;
-                    minInsertDist = insertDist;
-                }
-            });
-            app.nnPath.addComponent(nextNode);
-            current = nextNode;
-        }
-        app.nnPathLayer.redraw();
-        var km = app.nnPath.getLength()/1000 + "";
-        $("#nearest-neighbor-distance").text(km.substring(0, km.indexOf(".")) + " km");
-    };
-    app.recalcRoute = function() {
-        if($("#nearest-edge")[0].checked) {
-            app.neCalcRoute();
-        }
-        if($("#nearest-neighbor")[0].checked) {
-            app.nnCalcRoute();
-        }
-    };
-    var point = new OpenLayers.Control.DrawFeature(app.pointLayer, OpenLayers.Handler.Point, {
-        autoActivate: true
-    });
-    point.events.register("featureadded", {}, app.recalcRoute);
-    app.map.addControls([
-        point,
-        new OpenLayers.Control.DragFeature(app.pointLayer, {
-            onDrag: function() {
-                app.recalcRoute();
-            },
-            autoActivate: true
-        }),
-        new OpenLayers.Control.MousePosition({
-            displayProjection: "EPSG:4326",
-            div: $("#coords")[0]
-        })
-    ]);
-    $('#nearest-neighbor').button().click(function() {
-        if(this.checked) {
-            app.nnCalcRoute();
-        } else {
-            $("#nearest-neighbor-distance").text("");
-        }
-        app.nnPathLayer.setVisibility(this.checked);
-    });
-    $('#nearest-edge').button().click(function() {
-        if(this.checked) {
-            app.neCalcRoute();
-        } else {
-            $("#nearest-edge-distance").text("");
-        }
-        app.nePathLayer.setVisibility(this.checked);
-    });
+    d3.select(self.frameElement).style("height", height + "px");
     $('#wtf').button();
-    $("#github").button();
+    $('#github').button();
 });
